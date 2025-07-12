4b0c1c01e76a74b9f6df775335addc68ade9f7e2
Med Aziz Sahnoun <med.aziz@saveyourwardrobe.com>
2025-07-12 16:56:30 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,75 @@
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       strategies: "injectManifest",
-      srcDir: "src",            // Your custom service worker location
-      filename: "sw.js",        // Output service worker in dist/
-      injectRegister: false,
+      srcDir: "src",
+      filename: "sw.js",
       registerType: "autoUpdate",
-
-      // This part is critical: make sure globDirectory points to "dist"
+      injectRegister: false,
       injectManifest: {
-        globDirectory: "dist",
-        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-        globIgnores: ["sw.js"],     // Exclude your own sw.js file from precache
         maximumFileSizeToCacheInBytes: 5000000,
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
       },
-
       manifest: {
-        name: "Tik Pulse",
-        short_name: "Tik Pulse App",
-        description: "Tik Pulse",
-        theme_color: "#ffffff",
+        name: "TikPulse",
+        short_name: "TikPulse",
+        description: "TikPulse - TikPulse",
+        theme_color: "#0098c3",
+        background_color: "#065781",
+        display: "standalone",
+        orientation: "portrait",
+        id: "p4p.app",
+        categories: ["entertainment", "lifestyle", "social"],
         icons: [
           {
-            src: "/icons/icon-192x192.png",
+            src: "/icons/192x.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/icon-512x512.png",
+            src: "/icons/512x.png",
             sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/48x.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "/icons/96x.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "/icons/144x.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icons/384x.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icons/72x.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "/icons/152x.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/icons/128x.png",
+            sizes: "128x128",
             type: "image/png",
           },
         ],
