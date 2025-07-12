0b559bcc27b22820e16840c748d3a1f7fc382ce7
Med Aziz Sahnoun <med.aziz@saveyourwardrobe.com>
2025-07-12 17:14:12 +0100

    fix ?

--- src/sw.js
+++ src/sw.js
@@ -1,10 +1,67 @@
 /// <reference lib="webworker" />
 
-import { precacheAndRoute } from 'workbox-precaching';
-import { clientsClaim } from 'workbox-core';
+import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
+import { clientsClaim } from 'workbox-core'
+import { NavigationRoute, registerRoute } from 'workbox-routing'
 
-// No TypeScript declarations needed in JS file
-clientsClaim();
-precacheAndRoute(self.__WB_MANIFEST);
+// self.__WB_MANIFEST is the default injection point
+precacheAndRoute(self.__WB_MANIFEST)
 
-self.skipWaiting();
+// clean old assets
+cleanupOutdatedCaches()
+
+/** @type {RegExp[] | undefined} */
+let allowlist
+// in dev mode, we disable precaching to avoid caching issues
+if (import.meta.env.DEV)
+  allowlist = [/^\/$/]
+
+// to allow work offline
+registerRoute(new NavigationRoute(
+  createHandlerBoundToURL('index.html'),
+  { allowlist },
+))
+
+self.addEventListener('push', (event) => {
+  console.log('New notification', event);
+  const data = event.data ? JSON.parse(event.data.text()) : {};
+  console.log(data)
+  const title = data.title || 'Notification';
+  const options = {
+    body: data.body,
+    icon: '/icons/icon-192x192.png',
+    badge: '/icons/badge-72x72.png',
+    data: {
+      url: data.url || '/',
+    },
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const urlToOpen = event.notification.data?.url || '/';
+
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientsArr) => {
+      const matchingClient = clientsArr.find(client =>
+        client.url === new URL(urlToOpen, self.location.origin).href
+      );
+
+      if (matchingClient && 'focus' in matchingClient) {
+        return matchingClient.focus();
+      }
+
+      if (self.clients.openWindow) {
+        return self.clients.openWindow(urlToOpen);
+      }
+    })
+  );
+});
+
+
+
+
+self.skipWaiting()
+clientsClaim()
