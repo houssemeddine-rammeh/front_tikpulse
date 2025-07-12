666f15c27f49fdb4cea7c28bed4dcf3f54077268
Med Aziz Sahnoun <med.aziz@saveyourwardrobe.com>
2025-07-12 19:35:31 +0100

    final fixes

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: false,
+      enabled: true,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
